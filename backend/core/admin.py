31390a4a9abddffb0f40cf624b0d36160272e960
Sascha Gerspach <sascha.gerspach@gmx.de>
2026-04-15 18:32:22 +0200

    Models im Django-Admin registrieren
    - Alle 6 Models mit sinnvollen list_display, filter und search_fields
    - filter_horizontal fuer Team-Members/Leads
    - autocomplete_fields fuer SkillAssignment

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Department,
+    Employee,
+    Skill,
+    SkillAssignment,
+    SkillCategory,
+    Team,
+)
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent')
+    search_fields = ('name',)
+    list_filter = ('parent',)
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'email', 'user')
+    search_fields = ('last_name', 'first_name', 'email')
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department')
+    list_filter = ('department',)
+    search_fields = ('name',)
+    filter_horizontal = ('members', 'team_leads')
+
+
+@admin.register(SkillCategory)
+class SkillCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'parent')
+    search_fields = ('name',)
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    list_filter = ('category',)
+    search_fields = ('name',)
+
+
+@admin.register(SkillAssignment)
+class SkillAssignmentAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'skill', 'level', 'status', 'confirmed_at')
+    list_filter = ('status', 'level', 'skill__category')
+    search_fields = ('employee__last_name', 'employee__first_name', 'skill__name')
+    autocomplete_fields = ('employee', 'skill', 'confirmed_by')
