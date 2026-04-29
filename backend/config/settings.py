7cd17f761446d68221cbb0a9b84b6fd002fa5a08
Sascha Gerspach <sascha.gerspach@gmx.de>
2026-04-29 12:45:17 +0200

    Enhance CI pipeline with build step, system checks, and caching

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
